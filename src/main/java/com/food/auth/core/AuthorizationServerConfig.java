525172371c45932db4dedeb58ac4a3f553110d23
Renan Azzolim <azzolimrenan2@gmail.com>
2021-09-18 23:02:22 -0300

    23.36. Configurando os clientes OAuth2 em um banco de dados SQL

--- src/main/java/com/food/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/food/auth/core/AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package com.food.auth.core;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,46 +30,21 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Autowired
 	private AuthenticationManager authenticationManager;
 	
 	@Autowired
 	private UserDetailsService userDetailService;
 	
 	@Autowired
-	private JwtKeyStoreProperties jwtKeyStoreProperties; 
+	private JwtKeyStoreProperties jwtKeyStoreProperties;
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {		
-		clients
-			.inMemory()
-				.withClient("food-web")
-				.secret(passwordEncoder.encode("web123"))
-				.authorizedGrantTypes("password", "refresh_token")
-				.scopes("write", "read")
-				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
-				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
-			.and()
-				.withClient("food-analytics")
-				.secret(passwordEncoder.encode(""))
-				.authorizedGrantTypes("authorization_code")
-				.scopes("write", "read")
-				.redirectUris("http://aplicacao-cliente")
-			.and()
-				.withClient("webadmin")
-				.authorizedGrantTypes("implicit")
-				.scopes("write", "read")
-				.redirectUris("http://aplicacao-cliente")
-			.and()
-				.withClient("faturamento")
-				.secret(passwordEncoder.encode("faturamento123"))
-				.authorizedGrantTypes("client_credentials")
-				.scopes("read", "write")
-			.and()
-				.withClient("checktoken")
-				.secret(passwordEncoder.encode("check123"));		
+		clients.jdbc(dataSource);
 	}
 	
 	@Override
